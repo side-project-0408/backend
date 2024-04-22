4e0b335e3b919fc46ac2f28246903e77411b40ca
Yuri Choi <yurichoi0728@gmail.com>
2024-04-22 13:33:58 +0900

    2024.04.22 1.도메인 생성

--- src/main/java/com/example/side/domain/Comment.java
+++ src/main/java/com/example/side/domain/Comment.java
@@ -1,4 +1,32 @@
 package com.example.side.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
 public class Comment {
+
+    @Id @GeneratedValue
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "created_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
--- src/main/java/com/example/side/domain/Recruit.java
+++ src/main/java/com/example/side/domain/Recruit.java
@@ -1,4 +1,29 @@
 package com.example.side.domain;
 
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
 public class Recruit {
+
+    @Id @GeneratedValue
+    @Column(name = "recruit_id")
+    private Long recruitId;
+
+    @Column(name = "target_count")
+    private int targetCount;
+
+    @Column(name = "current_count")
+    private int currentCount;
+
+    @Column
+    private String position;
+
+
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
