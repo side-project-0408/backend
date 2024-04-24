b6d90e3cd06ecf0572ddf139b321387cc005134a
b3am11 <yurichoi0728@gmail.com>
2024-04-25 01:16:19 +0900

    2024.04.25 1. people 조회

---
+++ src/main/java/com/example/backend/dto/response/people/PeopleResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.backend.dto.response;
+
+public class PeopleResponseDto {
+
+    private String nickName;
+
+    private int favoriteCount;
+
+    private int viewCount;
+
+    private String position;
+
+    private String userFileUrl;
+}
---
+++ src/main/java/com/example/backend/service/PeopleService.java
@@ -0,0 +1,2 @@
+package com.example.backend.service;public class PeopleSservice {
+}
