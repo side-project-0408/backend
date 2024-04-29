c10c5ebfb4698e62a9cb484fd61c3f9973eec6bc
khm81712 <101914246+khm81712@users.noreply.github.com>
2024-04-29 14:49:13 +0900

    Update ProjectRepository.java

--- src/main/java/com/example/backend/repository/ProjectRepository.java
+++ src/main/java/com/example/backend/repository/ProjectRepository.java
@@ -1,15 +1,15 @@
 package com.example.backend.repository;
 
 import com.example.backend.domain.Project;
-import com.example.backend.dto.request.project.ProjectSearchDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     Optional<Project> findByUserIdAndProjectId(Long userId, Long projectId);
+
+    Project findByProjectId(Long projectId);
+
 }
